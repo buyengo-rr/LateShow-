3d0ac37f869329135d7a79b9a6e03305b18b2dd6
diagne <diagnebuyengo@gmail.com>
2025-07-08 13:15:03 +0300

    GET route returning list of episodes with id, date, and number

--- app.py
+++ app.py
@@ -8,4 +8,35 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 migrate = Migrate(app, db)
+@app.route('/episodes', methods=['GET'])
+def get_episodes():
+    episodes = Episode.query.all()
+    return jsonify([{"id": e.id, "date": e.date, "number": e.number} for e in episodes])
+
+
+@app.route('/episodes/<int:id>', methods=['GET'])
+def get_episode(id):
+    episode = Episode.query.get(id)
+    if not episode:
+        return jsonify({"error": "Episode not found"}), 404
+    return jsonify({
+        "id": episode.id,
+        "date": episode.date,
+        "number": episode.number,
+        "appearances": [
+            {
+                "id": a.id,
+                "rating": a.rating,
+                "guest_id": a.guest_id,
+                "episode_id": a.episode_id,
+                "guest": a.guest.to_dict()
+            } for a in episode.appearances
+        ]
+    })
+
+
+@app.route('/guests', methods=['GET'])
+def get_guests():
+    guests = Guest.query.all()
+    return jsonify([g.to_dict() for g in guests])
 
