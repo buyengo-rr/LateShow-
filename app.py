f688c9571e1e7198de27c2085c701584677a377a
diagne <diagnebuyengo@gmail.com>
2025-07-08 13:05:24 +0300

    database configuration and migrations

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+from flask import Flask, request, jsonify
+from flask_migrate import Migrate
+from models import db, Episode, Guest, Appearance
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lateshow.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)
 
